94d4d459a2121a73ee53bb1a530e241943da6430
Kimari-Rennis <kdr319@nyu.edu>
2022-11-04 17:29:21 -0400

    Sanity and Sprint UI Complete

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -14,29 +15,87 @@ public class PlayerMovement : MonoBehaviour
     private float originalSpeed;
     public float sprintSpeed;
 
+    //Tracking The Sprint
+    [SerializeField] Slider slider;
+    public float stamina;
+    public float maxStamina;
+    public float staminaDrain;
+    public float recovery;
+
+    //Sprint cooldown
+    public float cd;
+    public float originalcd;
+    public bool sprinting;
+    public bool canSprint;
 
     private void Start()
     {
         originalSpeed = moveSpeed;
+        sprinting = false;
+        canSprint = true;
+
+        cd = originalcd;
+
+        stamina = maxStamina;
     }
     // Update is called once per frame
     private void Update()
     {
         //Processsing Inputs
         ProcessInputs();
+
+        slider.value = stamina / 100;
+
+        if (sprinting == true && canSprint) //Currently Sprinting
+        {
+            stamina -= staminaDrain * Time.deltaTime; //Stamina drains
+        }
+        
+
+        if (stamina <= 0) //Empty Stamina
+        {
+            sprinting = false;
+            canSprint = false;
+        }
+
+        if (!canSprint)
+        {
+            cd -= 1 * Time.deltaTime;
+        }
+        
+        if(cd <= 0)
+        {
+            canSprint = true;
+            cd = originalcd;
+        }
+
+        if (sprinting == false) //If you can't sprint
+        {
+            stamina += recovery * Time.deltaTime; //Recover stamina
+        }
+
+
+
+        if (stamina >= maxStamina) //If you're at max Stamina you can't go over that amount
+        {
+            stamina = maxStamina;
+            
+        }
     }
 
     void FixedUpdate()
     {
         //Processing Calculations
         Move();
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKey(KeyCode.LeftShift) && canSprint)
         {
             Sprint();
+            sprinting = true;
         }
         else
         {
             originalSpeed = moveSpeed;
+            sprinting = false;
         }
 
     }
